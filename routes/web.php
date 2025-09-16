eccbe3c698323da77c7a9c5a9747e6dd2b89935a
uvenven <fadilefd1102@gmail.com>
2025-09-17 01:14:09 +0700

    Implement MetaMask authentication feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Web3AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Web3 / Metamask Authentication
+Route::prefix('eth')->group(function () {
+    Route::get('/signature', [Web3AuthController::class, 'signature']);
+    Route::post('/authenticate', [Web3AuthController::class, 'authenticate']);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
